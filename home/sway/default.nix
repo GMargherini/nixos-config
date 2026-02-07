531ccabaebb6508735136bdfbf2552ee937858b5
GMargherini <giorgio.margherini@gmail.com>
2026-02-07 14:59:59 +0100

    moved sway configs and changed monitors

--- home/sway/default.nix
+++ home/sway/default.nix
@@ -2,17 +2,17 @@
 {
   xdg.configFile = {
     "sway/config".source = lib.mkForce ./config;
-    "sway/config.d/containers.conf".source = ./config.d/containers.conf;
-    "sway/config.d/inputs.conf".source = ./config.d/inputs.conf;
-    "sway/config.d/layout.conf".source = ./config.d/layout.conf;
-    "sway/config.d/outputs.conf".source = ./config.d/outputs.conf;
+    "sway/config.d/containers.conf".source = ./containers.conf;
+    "sway/config.d/inputs.conf".source = ./inputs.conf;
+    "sway/config.d/layout.conf".source = ./layout.conf;
+    "sway/config.d/outputs.conf".source = ./outputs.conf;
   };
 
   wayland.windowManager.sway = {
     enable = true;
     package = pkgs.swayfx;
     wrapperFeatures.gtk = true;
-    config = rec {
+    config = {
       startup = [
         { command = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"; }
       ];
